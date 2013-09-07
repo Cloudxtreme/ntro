577d48bde1bec982055dcd665e2c21b87346f3a5
Petar Radosevic <petar@wunki.org>
2013-09-07 15:16:59 -0700

    Fix public and media directory

--- pimpin/urls.py
+++ pimpin/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -7,3 +10,7 @@ urlpatterns = patterns('',
     url('^', include('core.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# Add media and static files
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
