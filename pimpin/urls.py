2668cb35ee61729ca439ed83ad436af0f5356d9d
Petar Radosevic <petar@wunki.org>
2013-09-07 15:27:20 -0700

    Cleanup and remove core pages.

--- pimpin/urls.py
+++ pimpin/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url('^', include('core.urls')),
+    url(r'^',
+        TemplateView.as_view(template_name="index.html"),
+        name="index"),
     url(r'^admin/', include(admin.site.urls)),
 )
 
