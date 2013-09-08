f950c95c51181649eed7a8d33073e5483de74c91
Elmar Weber <home@elmarweber.org>
2013-09-07 21:56:00 -0700

    - implemented price lookup

--- pimpin/static/js/app/controller.js
+++ pimpin/static/js/app/controller.js
@@ -8,10 +8,10 @@ angular
             .when("/connections", {templateUrl: partialsUrl + "connections.html", controller: "ConnectionsCtrl"})
             .otherwise({redirectTo: "/"});
     })
-    .config(function($locationProvider) {
+    .config(function ($locationProvider) {
         $locationProvider.html5Mode(true);
     })
-    .controller("MakeConnectionCtrl", function($scope, Restangular, $timeout) {
+    .controller("MakeConnectionCtrl", function ($scope, Restangular, $timeout) {
         $scope.connection = {
             twitterHandle: null,
             pitch: null
@@ -21,36 +21,56 @@ angular
         $scope.lookupInProgress = false;
         $scope.person = null;
 
-        $scope.lookupPerson = function() {
+        $scope.lookupPerson = function () {
             $scope.lookupInProgress = true;
             $scope.person = null;
 
-            Restangular.one("person", $scope.connection.twitterHandle).get()
-                .then(function(person) {
-                    if (person === undefined) {
-                        $timeout(function() {
-                            $scope.lookupPerson();
-                        }, 1000);
-                    } else {
-                        $scope.person = person;
-                        $scope.step = "lookupSuccessful";
-                        $scope.lookupInProgress = false;
-                    }
-                }, function(response) {
+            Restangular.all("person").post({twitter_handle: $scope.connection.twitterHandle})
+                .then(function () {
+                    $scope.updatePerson();
+                }, function (response) {
                     $scope.error = "Person not found";
                     $scope.lookupInProgress = false;
                 });
         };
-        $scope.gotoPitch = function() {
-            $scope.step = "enterPitch";
+
+        $scope.updatePerson = function () {
+            Restangular.one("person", $scope.connection.twitterHandle).then(function (person) {
+                if (person.score && person.score !== null) {
+                    $scope.person = person;
+                    $scope.lookupInProgress = false;
+                    $scope.step = "lookupSuccessful";
+                } else {
+                    $timeout($scope.updatePerson, 1000);
+                }
+            });
         };
-        $scope.createConnection = function() {
-            $scope.step = "confirmation";
+
+
+        $scope.requestIntroduction = function () {
+            $scope.step = "requestIntroduction";
+            Restangular.all("connection").post({person: $scope.connection.twitterHandle})
+                .then(function (connection) {
+                    $scope.updateConnection(connection);
+                });
+        };
+
+        $scope.updateConnection = function (connection) {
+            Restangular.one("connection", connection.id).get().
+                then(function (connection) {
+                    if (connection.price && connection.price !== null) {
+                        $scope.connection = connection;
+                    } else {
+                        $timeout(function() {
+                            $scope.updateConnection(connection.id);
+                        }, 1000);
+                    }
+                });
         };
 
         $scope.topUsers = Restangular.all("user").getList();
     })
-    .controller("ConnectionsCtrl", function($scope, Restangular) {
+    .controller("ConnectionsCtrl", function ($scope, Restangular) {
         $scope.queryString = null;
         $scope.connections = Restangular.all("connection").getList();
     });
