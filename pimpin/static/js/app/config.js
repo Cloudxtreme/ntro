cf5e4847320ccaba5c8a70b67f05c2cd5413e1af
Elmar Weber <home@elmarweber.org>
2013-09-08 13:22:48 -0700

    - added enter handling

--- pimpin/static/js/app/config.js
+++ pimpin/static/js/app/config.js
@@ -8,4 +8,17 @@ angular
                 }
             }
         };
+    })
+    .directive('onEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.onEnter);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
     });
--- pimpin/static/js/app/controller.js
+++ pimpin/static/js/app/controller.js
@@ -5,13 +5,20 @@ angular
         $routeProvider
             .when("/", {templateUrl: partialsUrl + "home.html", controller: "MakeConnectionCtrl"})
             .when("/connection/pitch/:twitterHandle", {templateUrl: partialsUrl + "writePitch.html", controller: "WritePitchCtrl"})
-            .when("/connections", {templateUrl: partialsUrl + "connections.html", controller: "ConnectionsCtrl"})
+            .when("/profile", {templateUrl: partialsUrl + "connections.html", controller: "ConnectionsCtrl"})
             .when("/profile", {templateUrl: partialsUrl + "profile.html", controller: "ProfileCtrl"})
             .otherwise({redirectTo: "/"});
     })
-    .controller("MakeConnectionCtrl", function ($scope) {
+    .controller("MakeConnectionCtrl", function ($scope, $location) {
         $scope.isLoggedIn = pimp.user !== undefined;
         $scope.twitterHandle = null;
+
+        $scope.doSubmit = function() {
+            if ($scope.isLoggedIn) {
+                $location.path("/connection/pitch/" + $scope.normalizedTwitterHandle());
+            } else {
+            }
+        };
 
         $scope.normalizedTwitterHandle = function() {
             if ($scope.twitterHandle && $scope.twitterHandle !== null && $scope.twitterHandle.match(/^@.*/)) {
