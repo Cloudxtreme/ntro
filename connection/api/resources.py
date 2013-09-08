7937c00bfbe3ea6e94bc3d686db155c8db5a606a
Petar Radosevic <petar@wunki.org>
2013-09-07 22:57:53 -0700

    Creating an connection.

--- connection/api/resources.py
+++ connection/api/resources.py
@@ -5,9 +5,11 @@ from django.conf import settings
 from tastypie.resources import ModelResource
 from tastypie.validation import Validation
 from tastypie.authorization import Authorization
+from tastypie.authentication import ApiKeyAuthentication, Authentication
 from tastypie import fields
 from tastypie.http import HttpSeeOther
 from tastypie.exceptions import ImmediateHttpResponse
+
 
 from connection.models import Connection, Person
 
@@ -57,6 +59,7 @@ class PersonResource(ModelResource):
             pass
         else:
             raise ImmediateHttpResponse(HttpSeeOther("Person already exists"))
+
         return super(PersonResource, self).obj_create(bundle,
                                                       **kwargs)
 
@@ -73,14 +76,50 @@ class UserResource(ModelResource):
         return [
             url(r"^(?P<resource_name>%s)/(?P<username>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
         ]
-        
+
+#
+# Connection
+#
+class ConnectionValidation(Validation):
+    def is_valid(self, bundle, request=None):
+        if not bundle.data:
+            return {'__all__': 'At least supply us with the person.'}
+        if 'person' not in bundle.data:
+            return {'__all__': 'At least supply us with the person.'}
+
+class AuthenticatedPostAuthentication(ApiKeyAuthentication):
+    def is_authenticated(self, request, **kwargs):
+        """ If POST, don't check auth, otherwise fall back to parent """
+
+        if request.method == "GET":
+            return True
+        else:
+            return super(ApiKeyAuthentication, self).is_authenticated(request, **kwargs)
+            
 class ConnectionResource(ModelResource):
     """ Displaying all the connections """
-    person = fields.ToOneField(PersonResource, 'person', full=True)
-    requested_by = fields.ToOneField(UserResource, 'requested_by', full=True)
-    responded_by = fields.ToOneField(UserResource, 'responded_by', null=True, full=True)
-
+    person = fields.ForeignKey(PersonResource, 'person', full=True, blank=True)
+    requested_by = fields.ForeignKey(UserResource, 'requested_by', blank=True, full=True)
+    responded_by = fields.ForeignKey(UserResource, 'responded_by', blank=True, null=True)
+    
     class Meta:
         queryset = Connection.objects.all()
-        allowed_methods = ['get']
+        allowed_methods = ['get', 'post']
+        authentication = AuthenticatedPostAuthentication()
+        authorization = Authorization()
+        validation = ConnectionValidation()
+    
+    def obj_create(self, bundle, **kwargs):
+        bundle.data['requested_by'] = u'/user/%s/' % bundle.request.user
+        person = bundle.data['person'].split('/')[-2]
 
+        try:
+            Connection.objects.get(requested_by=bundle.request.user,
+                                   person__twitter_handle=person)
+        except Connection.DoesNotExist:
+            pass
+        else:
+            raise ImmediateHttpResponse(HttpSeeOther("Connection was already requested"))
+        
+        return super(ConnectionResource, self).obj_create(bundle,
+                                                          **kwargs)
