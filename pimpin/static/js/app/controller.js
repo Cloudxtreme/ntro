3266415ba7232e860deb18f1f2d9a62ea748852a
Elmar Weber <home@elmarweber.org>
2013-09-07 18:31:22 -0700

    - added restangular
    - basic rest list for connections

--- pimpin/static/js/app/controller.js
+++ pimpin/static/js/app/controller.js
@@ -1,21 +1,22 @@
 angular
-    .module("pimp.app", ["pimp.config"])
+    .module("pimp.app", ["pimp.config", "pimp.api"])
     .config(function ($routeProvider) {
+        var partialsUrl = "static/partials/"
         $routeProvider
-            .when("/home", {templateUrl:  "static/partials/" + "home.html", controller: "HomeCtrl"})
+            .when("/home", {templateUrl: partialsUrl + "home.html", controller: "HomeCtrl"})
+            .when("/connections", {templateUrl: partialsUrl + "connections.html", controller: "ConnectionsCtrl"})
             .otherwise({redirectTo: "/home"});
     })
     .controller("HomeCtrl", function ($scope, $http) {
         $scope.user = null;
 
-        $scope.auth = function () {
-            $http
-                .get("static/mock/" + "user.json")
-                .success(function (response) {
-                    $scope.user = response.user;
-                });
-        };
-
-
-        $scope.auth();
+        $http
+            .get("static/mock/" + "user.json")
+            .success(function (response) {
+                $scope.user = response.user;
+            });
+    })
+    .controller("ConnectionsCtrl", function($scope, Restangular) {
+        $scope.queryString = null;
+        $scope.connections = Restangular.all("connection").getList();
     });
